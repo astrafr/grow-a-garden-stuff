8e7f386e96fa9cd10aabb40081956fa121bd19aa
Astra <141445781+astrafr@users.noreply.github.com>
2025-07-09 10:57:22 -0400

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (e) {
+    console.error(e);
     res.status(500).json({ error: 'API Proxy Failed' });
   }
 }
